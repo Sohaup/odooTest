fc7bc41dc10cd33dd09450ba3eb6ca00fc25bde9
sohaip <sohybta560@gmail.com>
2025-07-19 16:29:27 +0300

    Update main.js
    fix some image path in js

--- js/main.js
+++ js/main.js
@@ -23,8 +23,8 @@ function domWrappMonad(element) {
 getHtmlElemnt("menu").map(toggleBgImage);
 function toggleBgImage(element) {    
     element.addEventListener("click" , function () {
-        if (getBgImageValue(element) != 'url("/imgs/download.svg")')
-        this.style.cssText="background-image: url('/imgs/download.svg');"
+        if (getBgImageValue(element) != 'url("../imgs/download.svg")')
+        this.style.cssText="background-image: url('../imgs/download.svg');"
         else 
         this.style.cssText=`background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%2833, 37, 41, 0.75%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");`
     })
